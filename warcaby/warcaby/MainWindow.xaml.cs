605e01b292265547c8d0298743fc05482f24430d
Damian Schmidt <damian.schmidt97@gmail.com>
2018-03-20 11:08:35 +0100

    moving between screens

--- warcaby/warcaby/MainWindow.xaml.cs
+++ warcaby/warcaby/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace warcaby
 {
@@ -27,17 +14,17 @@ namespace warcaby
 
         private void NewGame_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Board.IsSelected = true;
         }
 
         private void Ranking_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Ranking.IsSelected = true;
         }
 
         private void Close_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            Close();
         }
     }
 }
