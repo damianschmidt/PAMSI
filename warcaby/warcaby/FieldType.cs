33a1e10589dc9fcf3401eda0b1e484fd651c0427
Damian Schmidt <damian.schmidt97@gmail.com>
2018-03-23 15:07:37 +0100

    implementation of stopwatch and enum FieldType

---
+++ warcaby/warcaby/FieldType.cs
@@ -0,0 +1,17 @@
+﻿namespace warcaby
+{
+
+    /// <summary>
+    /// The type of value a cell in the game is currently at
+    /// </summary>
+    public enum FieldType
+    {
+        Free,
+        BlackPawn,
+        WhitePawn,
+        BlackQueen,
+        WhiteQueen,
+        SelectedPawn,
+        Move
+    }
+}
--- warcaby/warcaby/MainWindow.xaml.cs
+++ warcaby/warcaby/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace warcaby
 {
@@ -7,6 +11,10 @@ namespace warcaby
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        private Stopwatch stopWatch = new Stopwatch();
+        private string currentTime = string.Empty;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -15,6 +23,8 @@ namespace warcaby
         private void NewGame_Button_Click(object sender, RoutedEventArgs e)
         {
             Board.IsSelected = true;
+            dispatcherTimer.Start(); //Init stopwatch at board
+            stopWatch.Start();
         }
 
         private void Ranking_Button_Click(object sender, RoutedEventArgs e)
@@ -25,6 +35,30 @@ namespace warcaby
         private void Close_Button_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        //Board's functions
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (stopWatch.IsRunning)
+            {
+                TimeSpan ts = stopWatch.Elapsed;
+                currentTime = String.Format("{0:00}:{1:00}", ts.Minutes, ts.Seconds);
+
+                timer.Content = currentTime;
+            }
+        }
+
+        private void Restart_Button_Click(object sender, RoutedEventArgs e)
+        {
+            stopWatch.Restart();
         }
     }
 }
