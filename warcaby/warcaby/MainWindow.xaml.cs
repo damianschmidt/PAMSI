b887006a36b3040e1b0e5546cf2e9b6e1d514dec
Damian Schmidt <damian.schmidt97@gmail.com>
2018-03-26 01:34:43 +0200

    change concept of ButtonImage and NewGame function implementation

--- warcaby/warcaby/MainWindow.xaml.cs
+++ warcaby/warcaby/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
 using System.Windows.Threading;
 
 namespace warcaby
@@ -14,17 +19,80 @@ namespace warcaby
         private DispatcherTimer dispatcherTimer = new DispatcherTimer();
         private Stopwatch stopWatch = new Stopwatch();
         private string currentTime = string.Empty;
+        private FieldType[,] boardStatus;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void NewGame()
+        {
+            //Create a new blank array of free cells
+            boardStatus = new FieldType[8,4];
+
+            //init default value of fields on board
+            for(var i = 0; i < 3; i++)
+            {
+                for(var j = 0; j < 4; j++)
+                {
+                    boardStatus[i, j] = FieldType.BlackPawn;
+                    boardStatus[(7 - i), j] = FieldType.WhitePawn;
+                    if (i < 2)
+                    {
+                        boardStatus[(i + 3), j] = FieldType.Free;
+                    }
+                }
+            }
+
+            //Iterate every button on the grid
+            int k = 1;
+            FieldsGrid.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                if (k < 13)
+                {
+                    Uri resourceUri = new Uri(".\\img\\jpg\\checker-black.jpg", UriKind.Relative);
+                    StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
+                    BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+                    var brush = new ImageBrush();
+                    brush.ImageSource = temp;
+                    button.Background = brush;
+                    k++;
+                }
+                else if (k < 21)
+                {
+                    Uri resourceUri = new Uri(".\\img\\jpg\\field-dark.jpg", UriKind.Relative);
+                    StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
+                    BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+                    var brush = new ImageBrush();
+                    brush.ImageSource = temp;
+                    button.Background = brush;
+                    k++;
+                } else
+                {
+                    Uri resourceUri = new Uri(".\\img\\jpg\\checker-white.jpg", UriKind.Relative);
+                    StreamResourceInfo streamInfo = Application.GetResourceStream(resourceUri);
+                    BitmapFrame temp = BitmapFrame.Create(streamInfo.Stream);
+                    var brush = new ImageBrush();
+                    brush.ImageSource = temp;
+                    button.Background = brush;
+                }
+            });
+
+            //init stopwatch
+            dispatcherTimer.Start();
+            stopWatch.Start();
+
+            //set scores to 0
+            PlayerScore.Content = "0";
+            ComputerScore.Content = "0";
+
+        }
+
         private void NewGame_Button_Click(object sender, RoutedEventArgs e)
         {
             Board.IsSelected = true;
-            dispatcherTimer.Start(); //Init stopwatch at board
-            stopWatch.Start();
+            NewGame();
         }
 
         private void Ranking_Button_Click(object sender, RoutedEventArgs e)
@@ -36,6 +104,7 @@ namespace warcaby
         {
             Close();
         }
+
 
         //Board's functions
 
@@ -58,6 +127,7 @@ namespace warcaby
 
         private void Restart_Button_Click(object sender, RoutedEventArgs e)
         {
+            NewGame();
             stopWatch.Restart();
         }
     }
